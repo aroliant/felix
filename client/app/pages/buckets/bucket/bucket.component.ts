fed791cc8a7a83a35f7862262fbd480a6672eb6c
Jacob Samro <jacobsamro@outlook.com>
2019-11-06 20:10:24 +0530

    feat(client): implement basic file upload

--- client/app/pages/buckets/bucket/bucket.component.ts
+++ client/app/pages/buckets/bucket/bucket.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BucketComponent implements OnInit {
 
+  bucketName = 'aroliant'
+  currentPath = '/'
+
   constructor() { }
 
   ngOnInit() {
@@ -14,10 +17,38 @@ export class BucketComponent implements OnInit {
 
   uploadFiles(event) {
     console.log(event.target.files)
+    this.upload(event.target.files[0], 0)
   }
 
   uploadFolders(event) {
     console.log(event.target.files)
+  }
+
+  upload(file, uploadingIndex) {
+
+    console.log(file)
+
+    const fileLength = file.size;
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', `http://localhost:3000/bucket/objects/${this.bucketName}${this.currentPath}${file.name}`, true);
+    xhr.upload.onprogress = function (e) {
+      const percentComplete = Math.ceil((e.loaded / e.total) * 100);
+      console.log(percentComplete)
+    };
+
+    xhr.onload = function () {
+      if (this.status === 200) {
+        console.log(this.status)
+      }
+    }
+
+    if (fileLength > 0) {
+      xhr.send(formData);
+    }
   }
 
 
