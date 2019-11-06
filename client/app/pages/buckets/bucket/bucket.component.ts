ddd310ce4aa22bf8959999bb61406cff588fa4d1
Jacob Samro <jacobsamro@outlook.com>
2019-11-06 20:26:24 +0530

    feat(fileupload): implement both file & folder upload

--- client/app/pages/buckets/bucket/bucket.component.ts
+++ client/app/pages/buckets/bucket/bucket.component.ts
@@ -16,12 +16,21 @@ export class BucketComponent implements OnInit {
   }
 
   uploadFiles(event) {
-    console.log(event.target.files)
-    this.upload(event.target.files[0], 0)
+    const files = event.target.files
+
+    for (let i = 0; i < files.length; i++) {
+      this.upload(files[i], 0)
+    }
+
   }
 
   uploadFolders(event) {
-    console.log(event.target.files)
+    const files = event.target.files
+
+    for (let i = 0; i < files.length; i++) {
+      this.upload(files[i], 0)
+    }
+
   }
 
   upload(file, uploadingIndex) {
@@ -30,11 +39,21 @@ export class BucketComponent implements OnInit {
 
     const fileLength = file.size;
 
+    let uploadFilePath = ''
+
+    if (file.webkitRelativePath) {
+      uploadFilePath = `${this.bucketName}${this.currentPath}${file.webkitRelativePath}`
+    } else if (file.mozRelativePath) {
+      uploadFilePath = `${this.bucketName}${this.currentPath}${file.mozRelativePath}`
+    } else {
+      uploadFilePath = `${this.bucketName}${this.currentPath}${file.name}`
+    }
+
     const formData = new FormData();
     formData.append('file', file);
 
     const xhr = new XMLHttpRequest();
-    xhr.open('PUT', `http://localhost:3000/bucket/objects/${this.bucketName}${this.currentPath}${file.name}`, true);
+    xhr.open('PUT', `http://localhost:3000/bucket/objects/${uploadFilePath}`, true);
     xhr.upload.onprogress = function (e) {
       const percentComplete = Math.ceil((e.loaded / e.total) * 100);
       console.log(percentComplete)
