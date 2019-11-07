f6af0adaff6f089fd5c19a683476e0028b8ba750
Jacob Samro <jacobsamro@outlook.com>
2019-11-07 18:57:43 +0530

    add angular FormsModule

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +35,8 @@ import { BucketSettingsComponent } from './pages/buckets/bucket/bucket-settings/
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
