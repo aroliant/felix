ebeefb9cb0b829cc5dd97a03ae669687d79afb12
Jacob Samro <jacobsamro@outlook.com>
2019-11-11 18:10:11 +0530

    workaround for routerLinkActive issue in breadcrum

--- client/app/components/breadcrumb/breadcrumb.component.ts
+++ client/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent implements OnInit {
+export class BreadcrumbComponent implements OnInit, OnChanges {
 
   @Input() bucket: any
 
-  constructor() { }
+  isFilesTabActive = false
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+    if (!this.bucket) { return }
+
+    if (location.pathname === '/buckets/' + this.bucket.bucketName) {
+      this.isFilesTabActive = true
+    } else {
+      this.isFilesTabActive = false
+    }
   }
 
 }
