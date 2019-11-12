b267d0ef233f6d0581a5e2b27182a0571f28a2b7
Jacob Samro <jacobsamro@outlook.com>
2019-11-12 12:03:02 +0530

    feat(server): pipe to handle the bucket GET requests

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,5 +1,7 @@
-const express = require('express');
-
+import express from 'express';
+import fs from 'fs-extra'
+import path from 'path'
+import config from '../config';
 const router = express.Router(); // eslint-disable-line new-cap
 
 /** GET /health-check - Check service health */
@@ -8,5 +10,29 @@ router.get('/health-check', (req, res) =>
 );
 
 router.use('/bucket', require('./bucket.route'));
+
+// Pipe to handle GET Requests of Bucket Objects
+router.get('/:bucketName/**', (req, res) => {
+  const bucketName = req.params.bucketName
+  const relativePath = req.params[0]
+  const filePath = config.ROOT_FOLDER + "/buckets/" + bucketName + "/" + relativePath
+  const fileName = path.basename(filePath)
+  
+
+  fs.exists(filePath, function (exists) {
+    if (exists) {
+      res.writeHead(200, {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": "attachment; filename=" + fileName
+      });
+      return fs.createReadStream(filePath).pipe(res);
+    } else {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      return res.end("File does not exist");
+    }
+  });
+
+})
+
 
 module.exports = router;
