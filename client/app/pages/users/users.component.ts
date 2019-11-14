da35493bb6be0d4399674d6b08392f7496479054
Narendra Krishna Ram <narain@aroliant.com>
2019-11-14 11:34:44 +0530

    feat(users-ui): add delete user modal

--- client/app/pages/users/users.component.ts
+++ client/app/pages/users/users.component.ts
@@ -20,4 +20,11 @@ export class UsersComponent implements OnInit {
     document.getElementById("AddUserModal").style.display = "none";
   }
 
+  openDeleteUserModal() {
+    document.getElementById("DeleteUserModal").style.display = "block";
+  }
+  closeDeleteUserModal() {
+    document.getElementById("DeleteUserModal").style.display = "none";
+  }
+
 }
