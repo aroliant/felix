9a5513f86794e7dd69e7f65ddd02337924e306d9
Jacob Samro <jacobsamro@outlook.com>
2019-12-13 14:00:24 +0530

    feat(server): file sharing, custom headers, public / private

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import fs from 'fs-extra'
 import path from 'path'
+import low from 'lowdb'
+import FileSync from 'lowdb/adapters/FileSync'
+import crypto from 'crypto'
+
 import config from '../config';
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -20,15 +24,46 @@ router.use('/user', require('./user.route'));
 router.get('/:bucketName/**', (req, res) => {
   const bucketName = req.params.bucketName
   const relativePath = req.params[0]
+  const shareParam = req.query.share
   const filePath = config.ROOT_FOLDER + "/buckets/" + bucketName + "/" + relativePath
   const fileName = path.basename(filePath)
 
+  // Read the file's meta before sending the response
+  const metaFilePath = config.ROOT_FOLDER + "/meta/" + bucketName + "/" + relativePath.substring(0, relativePath.lastIndexOf('/')) + "/meta.json"
+
+  const adapter = new FileSync(metaFilePath)
+  const db = low(adapter)
+
+  const fileMeta = db.get('meta').get('children').find({ name: fileName }).value()
+
+  // Public / Private Restriction
+  if (!fileMeta.public && !shareParam) {
+    res.writeHead(403, { "Content-Type": "text/plain" });
+    return res.end("403 Forbidden")
+  }
+
+  // Check for File Sharing 
+  if (shareParam) {
+    // 1. Share Token
+    const shareToken = crypto.createHash('md5').update(fileMeta.sharingExpiresOn).digest('hex')
+
+    if (shareParam != shareToken) {
+      res.writeHead(403, { "Content-Type": "text/plain" });
+      return res.end("403 Forbidden: Invalid Share Token")
+    }
+    // 2. Check Share Expiry Time
+    if (fileMeta.sharingExpiresOn < Date.now()) {
+      res.writeHead(403, { "Content-Type": "text/plain" });
+      return res.end("403 Forbidden: Sharing Expired")
+    }
+  }
 
   fs.exists(filePath, function (exists) {
     if (exists) {
       res.writeHead(200, {
         "Content-Type": "application/octet-stream",
-        "Content-Disposition": "attachment; filename=" + fileName
+        "Content-Disposition": "attachment; filename=" + fileName,
+        ...fileMeta.meta // Append custom meta for Object
       });
       return fs.createReadStream(filePath).pipe(res);
     } else {
