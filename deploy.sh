b8c571224e0d0434c08f88a0aa6de6dee86c06ed
Jacob Samro <jacobsamro@outlook.com>
2019-12-26 16:54:50 +0530

    fix: tag :latest during push to dockerhub

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 VERSION=0.0.1-alpha.0
 docker build -f configs/docker/prod/Dockerfile . -t aroliant/felix:$VERSION 
+docker tag aroliant/felix:$VERSION aroliant/felix:latest
 docker push aroliant/felix:$VERSION
+docker push aroliant/felix:latest
